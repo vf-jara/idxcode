977d09733d1591fe5601fafb546dc3d3b6ceae7c
Vinicius Jara <viniciusfjara@gmail.com>
2022-05-05 16:07:50 -0400

    cria os componentes básicos da navbar

--- components/botaoOrcamento/BotaoOrcamento.jsx
+++ components/botaoOrcamento/BotaoOrcamento.jsx
@@ -3,7 +3,9 @@ import styles from './botaoOrcamento.module.css'
 export default function BotaoOrcamento(props) {
     return (
         <a href="#"><button name= "Solicite um Orçamento" className={styles.button}>
-            <span className={styles.text}>{props.texto}</span>
+            <span className={styles.text}>
+            {props.texto}
+            </span>
         </button></a>
     )
 }
--- components/menu/Menu.jsx
+++ components/menu/Menu.jsx
@@ -1,13 +1,17 @@
+import Link from 'next/link'
 import styles from './menu.module.css'
 
 export default function Menu() {
     return (
-        <div className={styles.menuContainer}>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/">O que Fazemos</a></li>
-                <li><a href="/">Quem Somos</a></li>
-            </ul>
-        </div>
+        <>
+            <div className={styles.menuContainer}>
+                <ul>
+                    <li><Link href="/"><a>Home</a></Link></li>
+                    <li><Link href="/"><a>O que Fazemos</a></Link></li>
+                    <li><Link href="/"><a>Quem Somos</a></Link></li>
+                </ul>
+            </div>
+
+        </>
     )
 }
