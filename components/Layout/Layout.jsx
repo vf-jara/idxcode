5f12194ac1df3c51e64013c53fc436ca8596fba8
Bernardo <eduardo.bernardo@indexdc.com>
2022-08-24 10:37:50 -0400

    Página Gestão de Tráfego

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -5,14 +5,6 @@ import Head from 'next/head';
 export default function Layout({ children }) {
     return (
         <>
-            <Head>
-                <meta charSet="utf-8" />
-                <title>Idxcode</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <meta property="og:description" content="Desenvolvimento de sites, gestão de tráfego e criação de chatbots. Cuidamos de toda tecnologia necessária para a sua empresa crescer no ambiente digital enquanto você se concentra em cuidar do seu negócio! " key="description" />
-                <meta name="description" content="Desenvolvimento de sites, gestão de tráfego e criação de chatbots. Cuidamos de toda tecnologia necessária para a sua empresa crescer no ambiente digital enquanto você se concentra em cuidar do seu negócio!" />
-            </Head>
-
             <Navbar />
             <main className="mt-20">{children}</main>
             <Footer />
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,11 +3,19 @@ import Services from "../components/serviceSection/Services";
 import Projects from "../components/projectsSection/Projects";
 import Contact from "../components/contactSection/Contact";
 import Script from 'next/script'
+import Head from "next/head";
 
 
 export default function Home() {
   return (
     <>
+      <Head>
+          <meta charSet="utf-8" />
+          <title>Idxcode</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta property="og:description" content="Desenvolvimento de sites, gestão de tráfego e criação de chatbots. Cuidamos de toda tecnologia necessária para a sua empresa crescer no ambiente digital enquanto você se concentra em cuidar do seu negócio! " key="description" />
+          <meta name="description" content="Desenvolvimento de sites, gestão de tráfego e criação de chatbots. Cuidamos de toda tecnologia necessária para a sua empresa crescer no ambiente digital enquanto você se concentra em cuidar do seu negócio!" />
+      </Head>
       <Hero/>
       <Services/>
       <Projects />
