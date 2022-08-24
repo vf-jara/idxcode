1bf59b6aeec183ad2f70bf94da691689d4347939
Bernardo <eduardo.bernardo@indexdc.com>
2022-08-24 13:39:07 -0400

    Página Gestão de Tráfego ajustes Desktop

--- pages/gestao-de-trafego.jsx
+++ pages/gestao-de-trafego.jsx
@@ -12,37 +12,39 @@ export default function Trafego() {
             <meta name="description" content="Apareça para as pessoas certas quando elas buscarem pelos seus produtos ou serviços online." />
         </Head>
         <div className="min-h-screen w-full bg-black">
-            <div className="flex flex-col gap-8 px-5 py-8 bg-[url('/assets/images/bg-trafego.png')] bg-cover">
-                <h1 className="text-white text-3xl font-semibold">Apareça para as <font className="text-idx-green font-bold">pessoas certas</font> quando elas buscarem pelo seu produto ou serviço <font className="text-idx-green font-bold">online</font>!</h1>
+            <div className="flex flex-col py-8 bg-[url('/assets/images/bg-trafego.png')] md:bg-[url('/assets/images/bg-desktop.png')] md:bg-bottom bg-cover">
+                <div className="flex flex-col gap-8 px-5 md:justify-center md:items-center md:mx-auto md:min-h-[50vh] md:w-8/12 md:gap-12 md:py-16 md:px-20 md:text-center">
+                    <h1 className="text-white text-3xl font-semibold md:text-5xl md:leading-snug md:mt-8">Apareça para as <font className="text-idx-green font-bold">pessoas certas</font> quando elas buscarem pelo seu produto ou serviço <font className="text-idx-green font-bold">on-line</font>!</h1>
 
-                <h2 className="text-white text-base font-normal leading-6">Existe um grande fluxo de pessoas passando pelas maiores <font className="text-idx-green font-bold">avenidas digitais</font> neste exato momento e olhando <font className="font-bold">vitrines</font> e <font className="font-bold">outdoors virtuais</font> de centenas de milhares de empresas.</h2>
-                <h2 className="text-white text-base leading-6 font-semibold">A sua empresa está sendo vista nessas <font className="text-idx-green font-bold">avenidas digitais</font>?</h2>
-                <Image width="40" height="40" objectFit="contain" className="opacity-75" src="/assets/images/scroll-bar@2x.png" alt="scroll" />
+                    <h2 className="text-white text-base font-normal leading-6 md:text-xl md:w-10/12 md:mt-8">Existe um grande fluxo de pessoas passando pelas maiores <font className="text-idx-green font-bold">avenidas digitais</font> neste exato momento e olhando <font className="font-bold">vitrines</font> e <font className="font-bold">outdoors virtuais</font> de centenas de milhares de empresas.</h2>
+                    <h2 className="text-white text-base leading-6 font-semibold md:text-2xl md:w-10/12 md:mt-8">A sua empresa está sendo vista nessas <font className="text-idx-green font-bold">avenidas digitais</font>?</h2>
+                    <div className="flex justify-center md:hidden"><Image width="40" height="40" objectFit="contain" className="opacity-75" src="/assets/images/scroll-bar@2x.png" alt="scroll" /></div>
+                </div>
             </div>
-            <div className="flex flex-col gap-8 px-5 py-8">
-                <p className="text-white text-base">Ainda hoje, <strong>90% dos empresários</strong> não perceberam que estão deixando um <font className="text-idx-green font-bold">caminhão de dinheiro na mesa</font> simplesmente por não acompanhar as mudanças do <font className="text-idx-green font-bold">mundo digital</font>.</p>
-                <p className="text-white text-base">O fato é que, possivelmente, te venderam a ideia de que bastava apenas ter uma página no <strong>Facebook</strong>, um perfil no <strong>Instagram</strong> e um <strong>site</strong> qualquer para aproveitar toda a onda do <strong>Marketing Digital</strong>.</p>
-                <p className="text-white text-base">Mas hoje, com o <strong>comportamento humano totalmente transformado pelo digital</strong>, acelerado especialmente nos últimos dois anos (C***D-19), não basta apenas ter um perfil nas redes sociais e um site meia-boca.</p>
-                <p className="text-white text-base font-bold">É preciso uma comunicação bem planejada e atrair pessoas para essas páginas.</p>
-                <p className="text-white text-base">Mas não é qualquer pessoa.</p>
-                <p className="text-white text-base font-bold">Você precisa atrair pessoas com <font className="text-idx-green">real potencial de compra</font>.</p>
-                <p className="text-white text-base">Pessoas <font className="text-idx-green font-bold">realmente interessadas</font> naquilo que você vende. Pessoas que, de fato, <font className="text-idx-green font-bold">vão comprar de você</font>.</p>
-                <p className="text-white text-base">A nossa função é encurtar o caminho desse público até você.</p>
-                <p className="text-white text-base font-bold">É fazer com que esse fluxo de pessoas, <font className="text-idx-green font-bold">com real interesse e potencial de compra chegue até você</font>.</p>
+            <div className="flex flex-col gap-8 px-5 py-8 md:justify-center md:items-center md:mx-auto md:w-8/12 md:gap-12 md:py-16 md:px-20 md:text-center">
+                <p className="text-white text-base md:text-lg">Ainda hoje, <strong>90% dos empresários</strong> não perceberam que estão deixando um <font className="text-idx-green font-bold">caminhão de dinheiro na mesa</font> simplesmente por não acompanhar as mudanças do <font className="text-idx-green font-bold">mundo digital</font>.</p>
+                <p className="text-white text-base md:text-lg">O fato é que, possivelmente, te venderam a ideia de que bastava apenas ter uma página no <strong>Facebook</strong>, um perfil no <strong>Instagram</strong> e um <strong>site</strong> qualquer para aproveitar toda a onda do <strong>Marketing Digital</strong>.</p>
+                <p className="text-white text-base md:text-lg">Mas hoje, com o <strong>comportamento humano totalmente transformado pelo digital</strong>, acelerado especialmente nos últimos dois anos (C***D-19), não basta apenas ter um perfil nas redes sociais e um site meia-boca.</p>
+                <p className="text-white text-base font-bold md:text-lg">É preciso uma comunicação bem planejada e atrair pessoas para essas páginas.</p>
+                <p className="text-white text-base md:text-lg">Mas não é qualquer pessoa.</p>
+                <p className="text-white text-base font-bold md:text-lg">Você precisa atrair pessoas com <font className="text-idx-green">real potencial de compra</font>.</p>
+                <p className="text-white text-base md:text-lg">Pessoas <font className="text-idx-green font-bold">realmente interessadas</font> naquilo que você vende. Pessoas que, de fato, <font className="text-idx-green font-bold">vão comprar de você</font>.</p>
+                <p className="text-white text-base md:text-lg">A nossa função é encurtar o caminho desse público até você.</p>
+                <p className="text-white text-base font-bold md:text-lg">É fazer com que esse fluxo de pessoas, <font className="text-idx-green font-bold">com real interesse e potencial de compra chegue até você</font>.</p>
             </div>
-            <div className="flex flex-col gap-8 px-5 py-8">
-                <h3 className="text-white text-2xl font-bold">Como isso é possível?</h3>
-                <p className="text-white text-base">Todo esse cenário digital funciona com base em quatro pilares: <font className="text-idx-green font-bold">Tráfego, Engajamento, Conversão e Retenção</font>.</p>
-                <p className="text-white text-base">O pilar responsável por fazer com que pessoas interessadas e com potencial de compra acessem seu site ou entrem em contato com a sua empresa é o <font className="text-idx-green font-bold">Tráfego</font>.</p>
-                <p className="text-white text-base">Através do <font className="text-idx-green font-bold">Tráfego Pago</font> (que são os <strong>anúncios online</strong>) conseguimos segmentar o público e entregar a sua mensagem para as pessoas certas, no momento ideal.</p>
-                <p className="text-white text-base">
+            <div className="flex flex-col gap-8 px-5 py-8 md:justify-center md:items-center md:mx-auto md:w-8/12 md:gap-12 md:py-16 md:px-20 md:text-center">
+                <h3 className="text-white text-2xl font-bold md:text-4xl">Como isso é possível?</h3>
+                <p className="text-white text-base md:text-lg">Todo esse cenário digital funciona com base em quatro pilares: <font className="text-idx-green font-bold">Tráfego, Engajamento, Conversão e Retenção</font>.</p>
+                <p className="text-white text-base md:text-lg">O pilar responsável por fazer com que pessoas interessadas e com potencial de compra acessem seu site ou entrem em contato com a sua empresa é o <font className="text-idx-green font-bold">Tráfego</font>.</p>
+                <p className="text-white text-base md:text-lg">Através do <font className="text-idx-green font-bold">Tráfego Pago</font> (que são os <strong>anúncios online</strong>) conseguimos segmentar o público e entregar a sua mensagem para as pessoas certas, no momento ideal.</p>
+                <p className="text-white text-base md:text-lg">
                     O <font className="text-idx-green font-bold">Tráfego Pago</font> é a <strong>estratégia</strong> que separa as empresas que apenas usam as redes sociais das empresas que utilizam as redes como <font className="text-idx-green font-bold">Canais de Venda Ativa</font>, capaz de gerar <strong>milhares de reais em faturamento</strong>.
                 </p>
             </div>
-            <div className="flex flex-col gap-8 px-5 py-8">
-                <h3 className="text-white text-2xl font-bold">Pra quem é?</h3>
-                <p className="text-white text-base">Nosso serviço é para <font className="text-idx-green font-bold">empresas físicas ou digitais</font>, com <strong>vontade de escalar vendas e faturamento de forma exponencial</strong>.</p>
-                <ul className="border-l-4 border-idx-green pl-3 py-1 flex flex-col gap-2">
+            <div className="flex flex-col gap-8 px-5 py-8 md:justify-center md:items-center md:mx-auto md:w-8/12 md:gap-12 md:py-16 md:px-20 md:text-center">
+                <h3 className="text-white text-2xl font-bold md:text-4xl">Pra quem é?</h3>
+                <p className="text-white text-base md:text-lg">Nosso serviço é para <font className="text-idx-green font-bold">empresas físicas ou digitais</font>, com <strong>vontade de escalar vendas e faturamento de forma exponencial</strong>.</p>
+                <ul className="border-l-4 border-idx-green pl-3 py-1 flex flex-col gap-2  md:text-lg">
                     <li className="text-white flex gap-2">
                         <img src="/assets/images/li.svg" alt="li" /> E-commerce
                     </li>
@@ -71,31 +73,31 @@ export default function Trafego() {
                         <img src="/assets/images/li.svg" alt="li" /> Energia Solar
                     </li>
                 </ul>
-                <p className="text-white text-base">E muito mais...</p>
-                <p className="text-white text-base">Independente do seu nicho, com a estratégia certa, o <font className="text-idx-green font-bold">Tráfego Pago</font> pode ser aplicado e trazer <font className="text-idx-green font-bold">resultados reais</font> e <font className="text-idx-green font-bold">mensuráveis</font> para a sua empresa.</p>
+                <p className="text-white text-base md:text-lg">E qualquer outra área de atuação...</p>
+                <p className="text-white text-base md:text-lg">Independente do seu nicho, com a estratégia certa, o <font className="text-idx-green font-bold">Tráfego Pago</font> pode ser aplicado e trazer <font className="text-idx-green font-bold">resultados reais</font> e <font className="text-idx-green font-bold">mensuráveis</font> para a sua empresa.</p>
             </div>
-            <div className="flex flex-col gap-8 px-5 py-8">
-                <h3 className="text-white text-2xl font-bold">Por que a idxcode?</h3>
-                <p className="text-white text-base">Nosso trabalho é orientado por entregar <font className="text-idx-green font-bold">resultados reais e mensuráveis</font> para nossos clientes.</p>
-                <p className="text-base font-bold text-idx-green">Aqui não tem achismo!</p>
-                <p className="text-white text-base"><strong>Todas as estratégias são testadas</strong>, com riscos controlados, para que nossos clientes tenham o <strong>máximo de resultados</strong> com o <strong>investimento adequado</strong>.</p>
-                <p className="text-white text-base">Ao longo dos últimos cinco anos trabalhando com <font className="text-idx-green font-bold">Gestão de Tráfego Pago</font>, somamos mais de <font className="text-idx-green font-bold">R$ 3,1 Milhões</font> em anúncios gerenciados nas principais plataformas de compra de mídia do mercado (Google Ads, Facebook Ads, Pinterest Ads, Linkedin Ads e outros canais de tráfego).</p>
-                <div className="flex flex-row gap-10" >
-                    <div className="border-l-4 border-idx-green pl-3 py-1 flex flex-col gap-1">
-                        <h3 className="font-bold text-idx-green text-2xl">R$3,1MM+</h3>
-                        <p className="text-white">Investidos</p>
+            <div className="flex flex-col gap-8 px-5 py-8 md:justify-center md:items-center md:mx-auto md:w-8/12 md:gap-12 md:py-16 md:px-20 md:text-center">
+                <h3 className="text-white text-2xl font-bold  md:text-4xl">Por que a idxcode?</h3>
+                <p className="text-white text-base md:text-lg">Nosso trabalho é orientado por entregar <font className="text-idx-green font-bold">resultados reais e mensuráveis</font> para nossos clientes.</p>
+                <p className="text-base font-bold text-idx-green md:text-2xl">Aqui não tem achismo!</p>
+                <p className="text-white text-base md:text-lg"><strong>Todas as estratégias são testadas</strong>, com riscos controlados, para que nossos clientes tenham o <strong>máximo de resultados</strong> com o <strong>investimento adequado</strong>.</p>
+                <p className="text-white text-base md:text-lg">Ao longo dos últimos cinco anos trabalhando com <font className="text-idx-green font-bold">Gestão de Tráfego Pago</font>, somamos mais de <font className="text-idx-green font-bold">R$ 3,1 Milhões</font> em anúncios gerenciados nas principais plataformas de compra de mídia do mercado (Google Ads, Facebook Ads, Pinterest Ads, Linkedin Ads e outros canais de tráfego).</p>
+                <div className="flex flex-row gap-10 md:gap-24" >
+                    <div className="border-l-4 border-idx-green pl-3 py-1 flex flex-col gap-1 text-left">
+                        <h3 className="font-bold text-idx-green text-2xl md:text-4xl">R$3,1MM+</h3>
+                        <p className="text-white md:text-lg">Investidos</p>
                     </div>
-                    <div className="border-l-4 border-idx-green pl-3 py-1 flex flex-col gap-1">
-                        <h3 className="font-bold text-idx-green text-2xl">R$16MM+</h3>
-                        <p className="text-white">Resultados</p>
+                    <div className="border-l-4 border-idx-green pl-3 py-1 flex flex-col gap-1 text-left">
+                        <h3 className="font-bold text-idx-green text-2xl md:text-4xl">R$16MM+</h3>
+                        <p className="text-white md:text-lg">Resultados</p>
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col gap-8 px-5 py-8">
-                <h3 className="text-white text-2xl font-bold">Quer fazer parte dos <font className="text-idx-green">10%</font> e escalar a sua empresa  <font className="text-idx-green">online</font>?</h3>
-                <p className="text-white text-base"><strong>Clique no <font className="text-idx-green font-bold">botão abaixo</font></strong>, preencha as informações necessárias e retornaremos com uma proposta adequada ao seu momento.</p>
-                <p className="text-white text-base font-bold">Estamos prontos para fazer a sua empresa crescer!</p>
-                <button className="w-full h-14 px-4 py-3 bg-idx-green text-black hover:border-2 hover:border-idx-green hover:text-idx-green hover:bg-black font-bold text-xl transition-all">Quero vender mais!</button>
+            <div className="flex flex-col gap-8 px-5 py-8 md:justify-center md:items-center md:mx-auto md:w-8/12 md:gap-12 md:py-16 md:px-20 md:text-center">
+                <h3 className="text-white text-2xl font-bold md:text-5xl">Quer fazer parte dos <font className="text-idx-green">10%</font> e escalar a sua empresa  <font className="text-idx-green">on-line</font>?</h3>
+                <p className="text-white text-base md:text-2xl"><strong>Clique no <font className="text-idx-green font-bold">botão abaixo</font></strong>, preencha as informações necessárias e retornaremos com uma proposta adequada ao seu momento.</p>
+                <p className="text-white text-base font-bold md:text-2xl">Estamos prontos para fazer a sua empresa crescer!</p>
+                <button className="w-full h-14 px-4 py-3 bg-idx-green text-black hover:border-2 hover:border-idx-green hover:text-idx-green hover:bg-black font-bold text-xl transition-all md:text-2xl md:h-20">Quero vender mais!</button>
             </div>
         </div>
     </>
