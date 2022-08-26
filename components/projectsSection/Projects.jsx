52ceda7454fbd6aa757c91d0dd2fbae22db3a080
Vinicius Jara <viniciusfjara@gmail.com>
2022-08-26 12:28:22 -0400

    removing stale animations

--- components/projectsSection/Projects.jsx
+++ components/projectsSection/Projects.jsx
@@ -1,28 +1,22 @@
 import SliderHome from '../slider/Slider'
 import BotaoOrcamento from '../botaoOrcamento/BotaoOrcamento'
-import { useInView } from 'react-intersection-observer'
-import { useCallback } from 'react'
 
 export default function Projects() {
-    const { ref, inView, entry } = useInView({
-        rootMargin:'0px',
-        threshold: 0.1,
-        triggerOnce: true,
-    })
+
 
     return (
         <>
             <div id="projects" className="w-full lg:flex lg:flex-row-reverse py-12 lg:pt-36">
 
                 <div className="lg:w-1/2 mx-[24px] lg:pl-24 pr-5 py-10">
-                    <div ref={ref} className={`${inView? "slideRight" : "opacity-0"}`}>
+                    <div className={``}>
                         <h2 className="title">Alguns projetos que já concluímos para<br /> nossos clientes</h2>
                     </div>
                     <div className="my-10">
-                        <p ref={ref} className={`text ${inView? "slideRight delay-1" : "opacity-0"}`}>Dos mais simples websites às mais complexas<br /> integrações com Inteligência Artificial.</p>
+                        <p className={`text`}>Dos mais simples websites às mais complexas<br /> integrações com Inteligência Artificial.</p>
                     </div>
 
-                    <div className={`${inView? "slideRight delay-2" : "opacity-0"}`}>
+                    <div className={``}>
                         <BotaoOrcamento texto="Solicite um orçamento" url="#contact" />
                     </div>
                 </div>
@@ -31,7 +25,7 @@ export default function Projects() {
                 </div>
             </div>
             <div className="w-full mx-auto bg-idx-bg-black">
-                <div ref={ref} className={`container mx-auto md:flex py-12 lg:py-24 ${inView? "slideRight" : "opacity-0"}`}>
+                <div className={`container mx-auto md:flex py-12 lg:py-24`}>
                     <div className="lg:w-1/2">
                         <h1 className="title text-white">Transformamos tecnologia em ativos digitais</h1>
                     </div>
